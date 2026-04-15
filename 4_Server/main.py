530f14586b3b4ad0764bef9696f30e67d2a43245
Andrew Hearts <JWTHDYTWA@yandex.ru>
2026-04-15 21:10:06 +0700

    Sync to PC

--- 4_Server/main.py
+++ 4_Server/main.py
@@ -4,43 +4,58 @@ import logging
 import psycopg
 import fastapi.responses as responses
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Depends, Request
 from contextlib import asynccontextmanager
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy import select
+
+
+class Base(DeclarativeBase):
+    ...
+
+
+class Product(Base):
+    __tablename__ = "products"
+    product_id: Mapped[int] = mapped_column(primary_key=True)
+    product_name: Mapped[str] = mapped_column()
+    product_price: Mapped[int] = mapped_column()
+    product_rating: Mapped[float] = mapped_column()
+    product_url: Mapped[str] = mapped_column()
 
 
 ROOT_DIR = os.path.dirname(__file__)
+DATABASE_URL = f"postgresql+asyncpg:///parser:{os.getenv('DB_PASS')}@localhost:5432/PDB"
 
-db_conninfo = (
-    "dbname=PDB "
-    "user=parser "
-    f"password={os.getenv('DB_PASS')} "
-    "host=localhost")
+engine = create_async_engine(DATABASE_URL)
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
-SQL_GET_PRODUCTS = """
-SELECT * FROM products
-"""
+async def get_async_session() -> AsyncSession: # type: ignore
+    async with async_session_maker() as session:
+        yield session
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     logger = logging.getLogger('uvicorn')
     logger.info('Password: %s', os.getenv('DB_PASS'))
     
-    with psycopg.connect(db_conninfo) as db_connection:
-        yield {'database': db_connection, 'logger': logger}
+    yield {'logger': logger}
+    await engine.dispose()
 
 
 app = FastAPI(lifespan=lifespan)
 
 @app.get("/")
-def root():
+async def root():
     return {'message': 'Hello world!'}
 
 @app.get("/ym")
-def get_market_prods(request: Request):
-    db_connection: psycopg.Connection = request.state.database
-    response = db_connection.execute(SQL_GET_PRODUCTS)
-    print(response)
-    return {'message': response.fetchall()}
+async def get_market_prods(database: AsyncSession = Depends(get_async_session)):
+    stmt = select(Product)
+    
+    resp = await database.execute(stmt)
+    return {'message': resp.all()}
 
 @app.get('/favicon.ico')
 def get_favicon():
